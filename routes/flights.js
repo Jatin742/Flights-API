a216d26c8b4840339e9305c936fc0379091c97de
Jatin Lalwani <112273774+Jatin742@users.noreply.github.com>
2023-05-18 17:06:07 +0530

    Second Commit

--- routes/flights.js
+++ routes/flights.js
@@ -4,12 +4,12 @@ const Flight = require('../models/Flight');
 const moment = require('moment');
 
 router.post('/', async (req, res) => {
-    const { source, destination, dateString } = req.body;
-    const date = moment(dateString, 'DD MMMM YYYY').format('YYYY-MM-DD[T]HH:mm:ss.SSS[Z]');
+    const { source, destination, date } = req.body;
+    const dateString = moment(date, 'DD MMMM YYYY').format('YYYY-MM-DD[T]HH:mm:ss.SSS[Z]');
     // const date = new Date(Date.parse(formattedDateString));
     
     try {
-        const flights = await Flight.find({ source, destination, date });
+        const flights = await Flight.find({ source, destination, "date":dateString });
         
         const prices = flights.reduce((result, flight) => {
             result[flight.airline] = `₹${flight.price}`;
